6942a9c70c2cfab637cac26aa15b6944114c9c85
XJBinder <115577036+XJBinder@users.noreply.github.com>
2024-07-17 16:36:20 -0400

    Add/Drop Course from Schedule Tests Working

--- Assignment6/binder/test_binder.py
+++ Assignment6/binder/test_binder.py
@@ -1,8 +1,6 @@
 import unittest
 import sqlite3
 import main
-import io
-from contextlib import redirect_stdout
 from unittest.mock import patch
 
 
@@ -10,87 +8,58 @@ class TestFunction(unittest.TestCase):
 
     @classmethod
     def setUp(cls):
-        # Create the COURSE table and add some test data
+        # Create the tables and add some test data
         cls.conn = sqlite3.connect('database.db')
         cls.cursor = cls.conn.cursor()
         main.create_COURSE_table()
-        main.create_STUDENT_table()
         main.create_STUDENT_COURSE_table()
-        main.create_INSTRUCTOR_table()
 
         # Clear existing data to avoid unique constraint violation
         cls.cursor.execute("DELETE FROM COURSE")
-        cls.cursor.execute("DELETE FROM STUDENT")
         cls.cursor.execute("DELETE FROM STUDENT_COURSE")
-        cls.cursor.execute("DELETE FROM INSTRUCTOR")
-        cls.conn.commit()
 
         # Add test data
         cls.cursor.execute("INSERT INTO COURSE (CRN, TITLE, DEPARTMENT, TIME, DAYS, SEMESTER, YEAR, CREDITS) VALUES"
-                           " (10001, 'How to Breathe', 'BSCE', '1530', 'MWF', 'Summer', 2024, 4)")
+                            " (10001, 'How to Breathe', 'BSCE', '1530', 'MWF', 'Summer', 2024, 4)")
         cls.cursor.execute("INSERT INTO COURSE (CRN, TITLE, DEPARTMENT, TIME, DAYS, SEMESTER, YEAR, CREDITS) VALUES"
-                           " (10002, 'Intro to Walking', 'BSEE', '0800', 'TTH', 'Summer', 2024, 4)")
+                            " (10002, 'Intro to Walking', 'BSEE', '0800', 'TTH', 'Summer', 2024, 4)")
         cls.cursor.execute("INSERT INTO STUDENT_COURSE (WENTWORTHID, CRN) VALUES ('W00409648', 10001)")
 
-        # Create test objects
-        cls.student = main.Student('W00409648', 'Jon', 'Binder', 'password', 'N/A', 2025, 'BSCE')
-        cls.instructor = main.Instructor('W60606060', 'Douglas', 'Dow', 'password', 'BSCE', 2005,
-                                         'Professor', 'ddow@wit.edu')
         cls.conn.commit()
 
+        # Create a student object
+        cls.student = main.Student('Jon', 'Binder', 'W00409648', 'password', major='BSCE', grad_year=2025)
+
     @classmethod
-    def tearDown(cls):
+    def tearDownClass(cls):
         # Drop the COURSE table after tests
         cls.cursor.execute("DROP TABLE IF EXISTS COURSE")
         cls.cursor.execute("DROP TABLE IF EXISTS STUDENT_COURSE")
+
         cls.conn.commit()
         cls.conn.close()
 
-    def test_student_add_course(self):
-        # Capture the output of the add_course Method
-        captured_output = io.StringIO()
-        with redirect_stdout(captured_output):
-            self.student.add_course()
-        output = captured_output.getvalue()
+    @patch('main.is_crn_unique',
+           return_value=(('10002', 'Intro to Walking', 'BSEE', '0800', 'TTH', 'Summer', 2024, 4), '10002'))
+    def test_student_add_course(self, mock_is_crn_unique):
+        # Run the drop course method
+        self.student.add_course()
 
-        # Check if Add Course is in the output
-        self.assertIn('\n--Add Course--', output)
+        # Verify the course was added to the database
+        self.cursor.execute("SELECT CRN FROM STUDENT_COURSE WHERE WENTWORTHID = 'W00409648' AND CRN = 10002")
+        course = self.cursor.fetchone()
+        self.assertIsNotNone(course)
 
-    def test_student_drop_course(self):
-        # Capture the output of the drop_course Method
-        captured_output = io.StringIO()
-        with redirect_stdout(captured_output):
-            self.student.drop_course()
-        output = captured_output.getvalue()
+    @patch('main.is_crn_unique',
+           return_value=(('10001', 'How to Breathe', 'BSCE', '1530', 'MWF', 'Summer', 2024, 4), '10001'))
+    def test_student_drop_course(self, mock_is_crn_unique):
+        # Run the drop course method
+        self.student.drop_course()
 
-        # Check if Drop Course is in the output
-        self.assertIn('\n--Drop Course--', output)
-
-    # @patch('builtins.input', side_effect=['', 'Math 101', '', ''])
-    # def test_search_roster(self, mock_input):
-    #     # Test searching by title
-    #     captured_output = io.StringIO()
-    #     with redirect_stdout(captured_output):
-    #         main.search_roster()
-    #     output = captured_output.getvalue()
-    #
-    #     # Check if the correct course is found
-    #     self.assertIn('Math 101', output)
-    #     self.assertNotIn('History 101', output)
-    #     self.assertNotIn('Biology 101', output)
-    #
-    # @patch('builtins.input', side_effect=['', '', 'MWF', ''])
-    # def test_show_roster(self, mock_input):
-    #     # Test searching by day
-    #     captured_output = io.StringIO()
-    #     with redirect_stdout(captured_output):
-    #         main.show_roster()
-    #     output = captured_output.getvalue()
-    #
-    #     # Check if the correct courses are found
-    #     self.assertIn('Math 101', output)
-    #     self.assertNotIn('History 101', output)
-    #     self.assertIn('Biology 101', output)
+        # Verify the course was removed from the database
+        self.cursor.execute("SELECT CRN FROM STUDENT_COURSE WHERE WENTWORTHID = 'W00409648' AND CRN = 10001")
+        course = self.cursor.fetchone()
+        self.assertIsNone(course)
 
 
 if __name__ == '__main__':
